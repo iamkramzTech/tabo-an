19d35bd11dbce196d0d6825c7f49a5e874549258
iamkramzTech <bernaldezm88@yahoo.com>
2023-11-16 22:54:46 +0800

    modified login.php

--- login.php
+++ login.php
@@ -1,4 +1,65 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/kramzcommerce/includes/header.php';?>
+
 <?php
 
-echo '<h1>Login Here</h1>'
 ?>
+
+
+
+<body class="d-flex align-items-center py-4 bg-body-tertiary">
+    <!-- <div class="container px-4 px-lg-5">
+      <div class="login-container"> -->
+    <div class="container px-4 px-lg-5">
+      <div class="d-flex justify-content-center">
+        <div class="card shadow-lg">
+          <div class="card-body px-4 py-5 px-md-5">
+            <main class="form-signin w-100 m-auto">
+              <img
+                class="mb-4"
+                src="/assets/bootstrap-logo.svg"
+                alt=""
+                width="72"
+                height="57"
+              />
+              <h1 class="h3 mb-3 fw-normal">Login</h1>
+              <form>
+                <div class="form-floating mb-4">
+                  <input
+                    class="form-control"
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="yourname@example.com"
+                    required
+                  />
+                  <label for="email">Email</label>
+                </div>
+                <div class="form-floating mb-4">
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="password"
+                    name="password"
+                    placeholder="Password"
+                    required
+                  />
+                  <label for="password">Password</label>
+                </div>
+                <button type="submit" class="btn btn-primary w-100 py-2">
+                  Login
+                </button>
+                <p>
+                  Don't have an account? <a href="signup.html">Register here</a>
+                </p>
+              </form>
+            </main>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  
+    
+    <!-- </div> -->
+
+    <!-- </div> -->
