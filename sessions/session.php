6211006593dd6c4ee022e6139280a15ad3c2a08a
iamkramzTech <bernaldezm88@yahoo.com>
2023-11-23 21:45:03 +0800

    Added functions in session php

---
+++ sessions/session.php
@@ -0,0 +1,50 @@
+<?php 
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/kramzcommerce/database/database.php');
+
+session_start();
+
+/*
+*NOTE: user_role
+* 0 = admin
+* 1 = vendor
+* 2 = customer
+*/
+//redirect(userid,role)
+function redirect($userId,$role) {
+
+ switch($role) {
+    case 0:
+        $_SESSION['admin'] = $userId;
+        header('Location:admin/dashboard');
+        break;
+    case 1:
+        //vendor
+        $_SESSION['vendor'] = $userId;
+        header('Location:merchants/dashboard');
+        break;
+
+    case 2:
+        //customer
+        $_SESSION['customer'] = $userId;
+        header('Location:user-profile');
+        break;
+    default:
+        //account type not found
+        header('Location:my-account');
+        exit();
+        
+ }
+}
+
+// Function to log out a user
+function logout() {
+    // Unset all session variables
+    $_SESSION = [];
+
+    // Destroy the session
+    session_destroy();
+    header('location:index');
+}
+
+?>
