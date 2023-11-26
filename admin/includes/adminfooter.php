b49d4915d21260876a79f7d544777542dc4e8e04
iamkramzTech <bernaldezm88@yahoo.com>
2023-11-26 17:35:38 +0800

    Adding jquery script

--- admin/includes/adminfooter.php
+++ admin/includes/adminfooter.php
@@ -1,8 +1,10 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
    <script src="../assets/js/bootstrap/bootstrap.bundle.min.js"></script>
 
       <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
       <script src="../assets/js/admindashboard.js"></script>
 
 <script>
@@ -18,4 +20,19 @@ const loadContent = (page)=>{
    xhttp.open("GET",page,true);
    xhttp.send();
 }
+
+$(function() {
+    console.log('Script is executed.');
+    
+    // Use event delegation for dynamically added elements
+    $(document).on('click', '.btn', function() {
+        console.log('Button clicked.');
+        // Show the "Add Brand" modal
+        $('#addBrandModal').modal('show');
+    });
+   //  $(document).on('click', '.close', function() {
+   //    $('#addBrandModal').modal('hide');
+   //  });
+});
 </script>
+
